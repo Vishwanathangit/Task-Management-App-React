a0133c4a31a96acf01a29e22d17d4e84ef0a47c7
Vishwanathangit <vishwanathan1720@gmail.com>
2025-05-15 23:39:53 +0530

    Updated

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+import Home from './pages/Home'
+import AddTask from './pages/AddTask'
+import EditTask from './pages/EditTask'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+
+function App() {
+ 
+ return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/AddTask' element={<AddTask />}></Route>
+         <Route path='/edit/:id' element={<EditTask />} />
+        <Route path='*' element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
